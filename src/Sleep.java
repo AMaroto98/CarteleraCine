88de21c96db038e374a08eb305167dfd8587a106
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-05-14 17:48:33 +0200

    Añadida clase para hacer pausas

---
+++ src/Sleep.java
@@ -0,0 +1,10 @@
+public class Sleep {
+
+    public static void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+    }
+}
